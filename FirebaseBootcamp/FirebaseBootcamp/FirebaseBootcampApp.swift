bc6fc817bf27b84ae6fb85aa8d77bd1abd48de73
Srikuty <sreekuttystar@gmail.com>
2025-05-26 17:00:21 +0400

    project setup added

---
+++ FirebaseBootcamp/FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by Sreekutty Maya on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
